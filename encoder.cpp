fa287b3e4295c073866e4a0d14748910db10d6e8
Artin <artin961@gmail.com>
2025-03-20 12:30:55 +0200

    Encoder and PID ok

--- encoder.cpp
+++ encoder.cpp
@@ -1,89 +1,66 @@
-#include "encoder.h"
+#include "Encoder.h"
 
 //------------------------------------------ class BUTTON ------------------------------------------------------
-BUTTON::BUTTON(uint16_t to = 3000) {
-  setTimeout(to);
+BUTTON::BUTTON(uint16_t to) : over_press(to) {}
+
+void BUTTON::init()
+{
+  ENCODER_DDR &= ~(ENCODER_BUTTON_BITMASK);
+  ENCODER_PORT |= ENCODER_BUTTON_BITMASK; // Enable pull-up
 }
 
-/*
-   The Encoder button current status
-   0    - not pressed
-   1    - short press
-   2    - long press
-*/
-void BUTTON::init(void) {
-  // ENSURE INPUTS
-  ENCODER_DDR &= ~ENCODER_DATA_BITMASK;
-  ENCODER_DDR &= ~ENCODER_BUTTON_BITMASK;
+uint8_t BUTTON::buttonCheck()
+{
+  uint32_t now = millis();
+  if (now < b_check)
+    return 0;
+  b_check = now + b_check_period;
 
-  // ACTIOVATE PULLUPS
-  ENCODER_PORT |= ENCODER_BUTTON_BITMASK;
-}
+  uint8_t s = (!(ENCODER_PIN & ENCODER_BUTTON_BITMASK)) ? (trigger_on << 1) : 0;
+  b_on = (b_on) ? (avg.average(s) >= trigger_off) : (avg.average(s) > trigger_on);
 
-uint8_t BUTTON::buttonCheck(void) {
-  if (millis() >= b_check) {                                  // It is time to check the button status
-    b_check = millis() + b_check_period;
-    uint8_t s = 0;
-    if (!(ENCODER_PIN & ENCODER_BUTTON_BITMASK))                               // if port state is low, the button pressed
-      s = trigger_on << 1;
-    if (b_on) {
-      if (avg.average(s) < trigger_off)
-        b_on = false;
-    } else {
-      if (avg.average(s) > trigger_on)
-        b_on = true;
+  if (!b_on)
+  {
+    if (bpt && !i_b_rel)
+    {
+      uint32_t press_duration = now - bpt;
+      bpt = 0;
+      i_b_rel = false;
+      return (press_duration < over_press) ? 1 : 0; // Short press
     }
-
-    if (b_on) {                                             // Button status is 'pressed'
-      uint32_t n = millis() - bpt;
-      if ((bpt == 0) || (n > over_press)) {
-        bpt = millis();
-      } else if (n > long_press) {                        // Long press
-        if (i_b_rel) {
-          return 0;
-        } else {
-          if (tick_period)
-            return buttonTick();
-          i_b_rel = true;
-          return 2;
-        }
-      }
-    } else {                                                // Button status is 'not pressed'
-      if (bpt == 0 || i_b_rel) {
-        bpt = 0;
-        i_b_rel = false;
-        return 0;
-      }
-      uint32_t e = millis() - bpt;
-      bpt = 0;                                            // Ready for next press
-      if (e < over_press) {                               // Long press already managed
-        return 1;
-      }
-    }
+    return 0;
   }
+
+  if (bpt == 0 || now - bpt > over_press)
+  {
+    bpt = now;
+  }
+  else if (now - bpt > long_press)
+  {
+    return (tick_period) ? buttonTick() : (i_b_rel = true, 2); // Long press
+  }
+
   return 0;
 }
 
-void BUTTON::setTimeout(uint16_t to) {
+void BUTTON::setTimeout(uint16_t to)
+{
   over_press = constrain(to, def_over_press, 5000);
   tick_period = 0;
 }
 
-bool BUTTON::setTick(uint16_t to) {
-  if (to > 0) {                                               // Setup tick period
-    tick_period = constrain(to, 100, 300);
-    over_press  = 16000;
-    return true;
-  } else {                                                    // disable tick
-    tick_period = 0;
-    over_press  = def_over_press;
-  }
-  return false;
+bool BUTTON::setTick(uint16_t to)
+{
+  tick_period = (to > 0) ? constrain(to, 100, 300) : 0;
+  over_press = (tick_period) ? 16000 : def_over_press;
+  return tick_period > 0;
 }
 
-uint8_t BUTTON::buttonTick(void) {
+uint8_t BUTTON::buttonTick()
+{
   uint32_t now_t = millis();
-  if (now_t - tick_time > tick_period) {
+  if (now_t - tick_time > tick_period)
+  {
     tick_time = now_t;
     return (bpt != 0);
   }
@@ -91,84 +68,63 @@ uint8_t BUTTON::buttonTick(void) {
 }
 
 //------------------------------------------ class ENCODER ------------------------------------------------------
-RENC::RENC(int16_t init_pos) : BUTTON() {
-  pt = 0; pos = init_pos;
-  min_pos = -32767; max_pos = 32766; ch_b = false; increment = 1;
-  changed = 0;
-  is_looped = false;
-}
+RENC::RENC(int16_t init_pos) : BUTTON(), pos(init_pos), min_pos(-32767), max_pos(32766) {}
 
-void RENC::init(void) {
+void RENC::init()
+{
   BUTTON::init();
 
+  ENCODER_DDR &= ~(ENCODER_DATA_BITMASK | ENCODER_CLOCK_BITMASK);
+  ENCODER_PORT |= ENCODER_DATA_BITMASK | ENCODER_CLOCK_BITMASK;
 
-
-  //pinMode(m_pin, INPUT_PULLUP);
-  //pinMode(s_pin, INPUT_PULLUP);
-
-  // ENSURE INPUTS
-  ENCODER_DDR &= ~ENCODER_DATA_BITMASK;
-  ENCODER_DDR &= ~ENCODER_CLOCK_BITMASK;
-  //ENCODER_DDR &= ~ENCODER_BUTTON_BITMASK;
-
-  // ACTIOVATE PULLUPS
-  ENCODER_PORT |= ENCODER_DATA_BITMASK;
-  ENCODER_PORT |= ENCODER_CLOCK_BITMASK;
-
-  // Configure external interrupt INT1 to trigger on any logical change
-  EICRA |= (1 << ISC10);  // ISC10=1 and ISC11=0 for triggering on logical change
-  EICRA &= ~(1 << ISC11);
-
-  // Enable the interrupt for INT1
-  EIMSK |= (1 << INT1);
-
+  EICRA = (EICRA & ~(1 << ISC11)) | (1 << ISC10); // Trigger on logical change
+  EIMSK |= (1 << INT1);                           // Enable external interrupt
 }
 
-bool RENC::write(int16_t init_pos) {
-  if ((init_pos >= min_pos) && (init_pos <= max_pos)) {
+bool RENC::write(int16_t init_pos)
+{
+  if (init_pos >= min_pos && init_pos <= max_pos)
+  {
     pos = init_pos;
     return true;
   }
   return false;
 }
 
-void RENC::reset(int16_t init_pos, int16_t low, int16_t upp, uint8_t inc, uint8_t fast_inc, bool looped) {
-  min_pos = low; max_pos = upp;
-  if (!write(init_pos)) init_pos = min_pos;
-  increment = fast_increment = inc;
-  if (fast_inc > increment) fast_increment = fast_inc;
+void RENC::reset(int16_t init_pos, int16_t low, int16_t upp, uint8_t inc, uint8_t fast_inc, bool looped)
+{
+  min_pos = low;
+  max_pos = upp;
+  if (!write(init_pos))
+    pos = min_pos;
+  increment = inc;
+  fast_increment = (fast_inc > increment) ? fast_inc : increment;
   is_looped = looped;
 }
 
-void RENC::encoderIntr(void) {                                  // Interrupt function, called when the channel A of encoder changed
-  bool rUp = (ENCODER_PIN & ENCODER_CLOCK_BITMASK);
-  unsigned long now_t = millis();
-  if (!rUp) {                                                 // The channel A has been "pressed"
-    if ((pt == 0) || (now_t - pt > over_press)) {
+void RENC::encoderIntr()
+{
+  uint32_t now_t = millis();
+  bool rUp = ENCODER_PIN & ENCODER_CLOCK_BITMASK;
+
+  if (!rUp)
+  {
+    if (!pt || now_t - pt > over_press)
+    {
       pt = now_t;
-      ch_b = (ENCODER_PIN & ENCODER_DATA_BITMASK);
+      ch_b = ENCODER_PIN & ENCODER_DATA_BITMASK;
     }
-  } else {
-    if (pt > 0) {
-      uint8_t inc = increment;
-      if ((now_t - pt) < over_press) {
-        if ((now_t - changed) < fast_timeout) inc = fast_increment;
-        changed = now_t;
-        if (ch_b) pos -= inc; else pos += inc;
-        if (pos > max_pos) {
-          if (is_looped)
-            pos = min_pos;
-          else
-            pos = max_pos;
-        }
-        if (pos < min_pos) {
-          if (is_looped)
-            pos = max_pos;
-          else
-            pos = min_pos;
-        }
-      }
-      pt = 0;
-    }
+  }
+  else if (pt)
+  {
+    uint8_t inc = ((now_t - changed) < fast_timeout) ? fast_increment : increment;
+    changed = now_t;
+    pos += ch_b ? -inc : inc;
+
+    if (pos > max_pos)
+      pos = (is_looped) ? min_pos : max_pos;
+    if (pos < min_pos)
+      pos = (is_looped) ? max_pos : min_pos;
+    pt = 0;
   }
 }
--- encoder.h
+++ encoder.h
@@ -1,56 +1,64 @@
 #ifndef _ENCODER_H_
 #define _ENCODER_H_
-#include "config.h"
+
 #include "EmpAvarage.h"
+#include "config.h"
 
 //------------------------------------------ class BUTTON ------------------------------------------------------
-class BUTTON {
-    public:
-        BUTTON(uint16_t timeout_ms = 3000);
-        uint8_t     buttonCheck(void);
-        void        setTimeout(uint16_t to = 3000);
-        bool        setTick(uint16_t to);
-        void        init(void);
-    private:
-        uint8_t     buttonTick(void);
-        EMP_AVERAGE         avg;                            // Do average the button readings to mainain the button status
-        uint16_t            over_press;                     // Maximum time in ms the button can be pressed
-        volatile bool       i_b_rel         = false;        // Ignore button release event
-        bool                b_on            = false;        // The button current position: true - pressed
-        uint32_t            bpt             = 0;            // Time in ms when the button was pressed (press time)
-        uint32_t            b_check         = 0;            // Time in ms when the button should be checked
-        uint32_t            tick_time       = 0;            // Time in ms when the last 'tick' was generated
-        uint16_t            tick_period     = 0;            // Repeat 'tick' period
-        const uint8_t       trigger_on      = 60;          // avg limit to change button status to on
-        const uint8_t       trigger_off     = 30;           // avg limit to change button status to off
-        const uint8_t       avg_length      = 4;            // avg length
-        const uint8_t       b_check_period  = 20;           // The button check period, ms
-        const uint16_t      long_press      = 1500;         // If the button was pressed more that this timeout, we assume the long button press
-        const uint16_t      def_over_press  = 2500;         // Default value for button overpress timeout (ms)
+class BUTTON
+{
+  public:
+    explicit BUTTON(uint16_t timeout_ms = 3000);
+    uint8_t buttonCheck();
+    void setTimeout(uint16_t to = 3000);
+    bool setTick(uint16_t to);
+    void init();
+
+  private:
+    uint8_t buttonTick();
+    EMP_AVERAGE avg; // Average button readings
+    uint16_t over_press;
+    volatile bool i_b_rel = false;
+    bool b_on = false;
+    uint32_t bpt = 0, b_check = 0, tick_time = 0;
+    uint16_t tick_period = 0;
+    static constexpr uint8_t trigger_on = 60;
+    static constexpr uint8_t trigger_off = 30;
+    static constexpr uint8_t avg_length = 4;
+    static constexpr uint8_t b_check_period = 20;
+    static constexpr uint16_t long_press = 1500;
+    static constexpr uint16_t def_over_press = 2500;
 };
 
 //------------------------------------------ class ENCODER ------------------------------------------------------
-class RENC : public BUTTON {
-    public:
-        RENC(int16_t init_pos = 0);
-        void        init(void);
-        void        set_increment(uint8_t inc)      { increment = inc; }
-        uint8_t     get_increment(void)             { return increment; }
-        int16_t     read(void)                      { return pos; }
-        void        reset(int16_t init_pos, int16_t low, int16_t upp, uint8_t inc = 1, uint8_t fast_inc = 0, bool looped = false);
-        bool        write(int16_t initPos);
-        void        encoderIntr(void);
-    private:
-        int32_t     min_pos, max_pos;
-        bool        is_looped;                      // Whether the encoder is looped
-        uint8_t     increment;                      // The value to add or subtract for each encoder tick
-        uint8_t     fast_increment;                 // The value to change encoder when in runs quickly
-        volatile uint32_t   pt;                     // Time in ms when the encoder was rotaded
-        volatile uint32_t   changed;                // Time in ms when the value was changed
-        volatile bool       ch_b;
-        volatile int16_t    pos;                    // Encoder current position
-        const uint16_t      fast_timeout    = 300;  // Time in ms to change encoder quickly
-        const uint16_t      over_press      = 1000;
+class RENC : public BUTTON
+{
+  public:
+    explicit RENC(int16_t init_pos = 0);
+    void init();
+    void set_increment(uint8_t inc) {
+      increment = inc;
+    }
+    uint8_t get_increment() const {
+      return increment;
+    }
+    int16_t read() const {
+      return pos;
+    }
+    void reset(int16_t init_pos, int16_t low, int16_t upp, uint8_t inc = 1, uint8_t fast_inc = 0, bool looped = false);
+    bool write(int16_t init_pos);
+    void encoderIntr();
+
+  private:
+    volatile int16_t pos;
+    int32_t min_pos;
+    int32_t max_pos;
+    bool is_looped = false;
+    uint8_t increment = 1, fast_increment = 0;
+    volatile uint32_t pt = 0, changed = 0;
+    volatile bool ch_b = false;
+    static constexpr uint16_t fast_timeout = 300;
+    static constexpr uint16_t over_press = 1000;
 };
 
 #endif
